5c881012fee3b2798a2aaceff029bc1e1b6c28de
mrmbs1 <mbilalsadiq.19@gmail.com>
2025-03-19 13:23:23 +0000

    counter app

---
+++ index.js
@@ -0,0 +1,17 @@
+// document.getElementById("count-el").innerText = 5
+
+// let firstBatch = 5
+// let secondBatch = 7
+
+// let count = firstBatch + secondBatch
+
+// console.log(count)
+
+let myAge = 26
+let humanDogRation = 7
+
+let myDogAge = myAge * humanDogRation
+
+console.log(myDogAge)
+ 
+
