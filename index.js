bcb3dcb590e65ce7cfbf5904c03925a78a1cb4d3
mrmbs1 <mbilalsadiq.19@gmail.com>
2025-04-07 21:43:23 +0100

    previous entry

--- index.js
+++ index.js
@@ -1,45 +1,34 @@
-// document.getElementById("count-el").innerText = 5
 
-// initialise the count as 0
-// listen for clicks on the increment button
-// increment the count variable when the button is clicked
-// change the count-el in the html to reflect the new count
+let countEl = document.getElementById("count-el")
+let count = 0 
 
-// function increment() {
-//     console.log("the button was clicked")
-// }
-
-
-// let countEl = document.getElementById("count-el")
-
-// let count = 0 
-
-// function increment() {
-//     count = count + 1
-//     countEl.innerText = count
+function increment() {
+    count += 1
+    countEl.innerText = count
     
-// }
-
-// function save() {
-//     console.log(count)
-// }
-
-// save()
-
-// let message = "You have three new notifications"
-
-// console.log(message)
-
-let welcomeEl = document.getElementById("welcome-el")
-
-let myName = "Bilaal"
-let greeting = "Welcome Back "
-
-
-
-function welcome() {
-    myGreeting = greeting + myName + " 👋🏻"
-    welcomeEl.innerText = myGreeting
 }
 
-welcome()
+function save() {
+
+
+    
+    console.log(count)
+}
+
+save()
+
+ 
+
+// let welcomeEl = document.getElementById("welcome-el")
+
+// let myName = "Bilaal"
+// let greeting = "Welcome Back "
+
+
+
+// function welcome() {
+//     myGreeting = greeting + myName + " 👋🏻"
+//     welcomeEl.innerText = myGreeting
+// }
+
+// welcome()
